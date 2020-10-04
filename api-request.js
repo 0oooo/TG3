12a1b6cf4729e219a1da194173c5dc38ffe4bd9c
Camille Croci <seh.camille@gmail.com>
2020-10-04 20:16:00 +0200

    get the info from API request

--- api-request.js
+++ api-request.js
@@ -1,7 +1,14 @@
+const subjects = ["football", "uknews", "travel"];
+const apiKey = "9wur7sdh84azzazdt3ye54k4";
+const mainUrl = "https://content.guardianapis.com/search?q=";
+const articles = {};
+
 window.addEventListener("load", () => {
     const selectedTab = document.getElementsByClassName("selected")[0];
     const id = selectedTab.id.replace("header", "");
     document.getElementById(id).style.display = "block";
+
+    getLatestNews();
 });
 
 const changeSelectedTab = (clickedTab) => {
@@ -15,4 +22,36 @@ const changeSelectedTab = (clickedTab) => {
 
     document.getElementById(oldId).style.display = "none";
     document.getElementById(currId).style.display = "block";
+};
+
+const getLatestNews = async () => {
+    for(let subject of subjects) {
+        let url = mainUrl + subject + "&api-key=" + apiKey;
+        let response = await fetch(url);
+        if (response.ok) {
+            let json = await response.json();
+            let results = json.response.results;
+            const currArticle = [];
+            for(let result of results){
+                const title = result["webTitle"];
+                const webUrl = result["webUrl"];
+                currArticle.push([title, webUrl]);
+            }
+            articles[subject] =  currArticle;
+        } else {
+            alert("HTTP-Error: " + response.status);
+        }
+    }
+    populateCurrHtml();
+    //To use
+    // webTitle be displayed
+    // trailText be displayed
+    // webTitle linked to webUrl
+};
+
+const populateCurrHtml = () => {
+    console.log(articles);
+    for(let subject of subjects){
+        console.log(articles[subject]);
+    }
 };
