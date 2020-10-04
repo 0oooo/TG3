290abfbaff9eed29229ca5124910a6b1ede82482
Camille Croci <seh.camille@gmail.com>
2020-10-04 19:42:04 +0200

    show content depending on selected tab

--- api-request.js
+++ api-request.js
@@ -1,8 +1,18 @@
+window.addEventListener("load", () => {
+    const selectedTab = document.getElementsByClassName("selected")[0];
+    const id = selectedTab.id.replace("header", "");
+    document.getElementById(id).style.display = "block";
+});
+
 const changeSelectedTab = (clickedTab) => {
     const selectedTab = document.getElementsByClassName("selected")[0];
 
-    // selectedTab.removeAttribute("id");
     selectedTab.classList.remove("selected");
-    clickedTab.classList.add("selected")
-    // clickedTab.setAttribute("id", "selected");
+    clickedTab.classList.add("selected");
+
+    const oldId = selectedTab.id.replace("header", "");
+    const currId = clickedTab.id.replace("header", "");
+
+    document.getElementById(oldId).style.display = "none";
+    document.getElementById(currId).style.display = "block";
 };
